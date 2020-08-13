7436a8d98afeb8772375e8736ed7f190a9b66d97
Mark Shields <laebshade@gmail.com>
2020-08-13 12:59:37 -0400

    36 ms

--- solutions/1480. Running Sum of 1d Array.py
+++ solutions/1480. Running Sum of 1d Array.py
@@ -1,13 +1,4 @@
 class Solution:
     def runningSum(self, nums: List[int]) -> List[int]:
-        
-        def inner(sums: List[int], left: List[int]) -> List[int]:
-            if not left:
-                return sums
-​
-            if sums:
-                return inner(sums + [sums[-1] + left[0]], left[1:])
-​
-            return inner([left[0]], left[1:])
-        
-        return inner([], nums)
+        from itertools import accumulate
+        return accumulate(nums)
