319f8568678de1e36aa3cfa43a10f75e285040ba
Mark Shields <laebshade@gmail.com>
2020-08-17 15:23:10 -0400

    52 ms

--- solutions/1165. Single-Row Keyboard.py
+++ solutions/1165. Single-Row Keyboard.py
@@ -3,9 +3,10 @@ from typing import Generator
 ​
 class Solution:
     def calculateTime(self, keyboard: str, word: str) -> int:
-        def inner(i: int, total: List[int], left: str):
-            if not left:
-                return total
-            j = keyboard.index(left[0])
-            return inner(i=j, total=total + [abs(i - j)], left=left[1:])
-        return sum(inner(0, [], word))
+        def inner():
+            i = 0
+            for s in word:
+                j = keyboard.index(s)
+                yield abs(i - j)
+                i = j
+        return sum(inner())
