81f2e1c047d4ec6b9735294fa2400b54fd6dfd21
Mark Shields <laebshade@gmail.com>
2020-08-19 07:36:55 -0400

    68 ms

--- solutions/1470. Shuffle the Array.py
+++ solutions/1470. Shuffle the Array.py
@@ -4,12 +4,10 @@ from itertools import chain
 ​
 class Solution:
     def shuffle(self, nums: List[int], n: int) -> List[int]:
-        def inner(l: List[int]) -> List[int]:
+        def inner() -> List[int]:
             d = defaultdict(list)
-            while l: 
+            while nums: 
                 for m in range(1, n + 1):
-                    d[m].append(l.pop(0))
+                    d[m].append(nums.pop(0))
             return list(chain.from_iterable(d.values()))
-            
-               
-        return inner(nums)
+        return inner()
