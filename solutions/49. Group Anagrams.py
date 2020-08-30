205ee2818b3880679b326a7271fc7ff3ff3cab2e
Mark Shields <laebshade@gmail.com>
2020-08-30 15:27:33 -0400

    100 ms

--- solutions/49. Group Anagrams.py
+++ solutions/49. Group Anagrams.py
@@ -1,15 +1,10 @@
-from collections import defaultdict
-from typing import Dict, List
-​
 class Solution:
     def groupAnagrams(self, strs: List[str]) -> List[List[str]]:
-        def inner(d: Dict[str, List[str]], left: List[str]) -> List[List[str]]:
-            if not left:
-                return d.values()
-            d["".join(sorted(left[0]))].append(left[0])
-            return inner(d, left[1:])
-        
-        d: Dict[str, List[str]] = defaultdict(list)
-        return inner(d, strs)
+        left = collections.deque(strs)
+        d = defaultdict(list)
+        while left:
+            word = left.pop()
+            d["".join(sorted(word))].append(word)
+        return d.values()
                 
             
