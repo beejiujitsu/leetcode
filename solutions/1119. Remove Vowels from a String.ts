20541d1dab7ed79cadeb8a09bdd951f408119989
Mark Shields <laebshade@gmail.com>
2020-09-09 12:07:27 -0400

    72 ms

--- solutions/1119. Remove Vowels from a String.ts
+++ solutions/1119. Remove Vowels from a String.ts
@@ -1,5 +1,3 @@
 function removeVowels(S: string): string {
-    let vowels = /[aeiou]+/gi;
-    let no_vowels = S.replace(vowels, "");
-    return no_vowels;
+    return S.replace(/[aeiou]/gi, "");
 };
